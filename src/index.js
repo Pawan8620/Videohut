3e3cd440a5db310a2dfb4ea9874ec3f7f0fb97e3
Pawankumar Pandey <pawanpandey86.20@gmail.com>
2024-11-03 21:06:19 +0530

    jwt bcypt added

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 7000,()=>{
+        console.log(`Server is running at PORT: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!",err)
+})
 
 
 
