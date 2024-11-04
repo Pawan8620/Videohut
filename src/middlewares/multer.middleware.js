86d4020b0e66b328c46ed548920d560a88cc9446
Pawankumar Pandey <pawanpandey86.20@gmail.com>
2024-11-04 19:57:31 +0530

    file handling done with multer and cloudinary configuration

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from 'multer'
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+      //if there are multiple file uploaded by user with same it will get overwrite but in this the file will be there on server for very tiny time therefore it doesnt affect it
+    }
+  })
+  
+  const upload = multer({ 
+    storage,//there is middleware with the name of storage
+})
